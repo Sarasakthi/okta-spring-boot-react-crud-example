04a4d6c13e2ea4dfc9d1ea99c0bd709725b7a44d
Sarasakthi <saraswathi1404@gmail.com>
2023-12-27 13:05:00 -0700

    from IntelliJ

---
+++ src/test/java/com/tarunjai/developer/reactspringbootdemo/ReactspringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarunjai.developer.reactspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactspringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
